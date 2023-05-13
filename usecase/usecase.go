4640d99678f3e9f729a9ba201498441275c774c0
Qingyun Hunang <qingyun.huang@ubitus.net>
2023-05-13 21:18:15 +0800

    refactor

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -44,47 +44,33 @@ func QueryByTag(path, tag string) error {
 	// Walk through all files and append path which contains '.go'
 	filesPath, err := getFilesPath(path)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
-	// Add file name to tag key
-	tagFiles := map[string][]string{}
+	// find the file which contains target tag
+	files := []string{}
 	for _, path := range filesPath {
-		content, err := os.ReadFile(path)
+		fileTags, hasTags, err := extractTags(path)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		fileContent := string(content)
-		lines := strings.Split(fileContent, "\n")
-
-		// if not set tag, continue to next file
-		if len(lines) < 3 {
-			continue
-		}
-
-		thirdLine := strings.TrimSpace(lines[2])
-		if strings.HasPrefix(thirdLine, "/* @tags:") && strings.HasSuffix(thirdLine, "*/") {
-			tagsLine := strings.TrimPrefix(thirdLine, "/* @tags:")
-			tagsLine = strings.TrimSuffix(tagsLine, "*/")
-			tagsLine = strings.TrimSpace(tagsLine)
-			tags := strings.Split(tagsLine, ",")
-			for _, tag := range tags {
-				if _, ok := tagFiles[tag]; !ok {
-					tagFiles[tag] = []string{}
+		if hasTags {
+			for _, fileTag := range fileTags {
+				if fileTag == tag {
+					files = append(files, path)
 				}
-				tagFiles[tag] = append(tagFiles[tag], path)
 			}
 		}
 	}
 
-	fmt.Println(tagFiles["abc"])
+	fmt.Println(files)
 
 	return nil
 }
 
 func QueryByExpression(path, expression string) error {
 	// '', -
-	// 'divide and conquer' AND tree
+	// 'divide and conquer' tree
 	expression = strings.TrimPrefix(expression, " ")
 	expression = strings.TrimSuffix(expression, " ")
 
@@ -128,9 +114,60 @@ func QueryByExpression(path, expression string) error {
 		}
 	}
 
-	fmt.Println()
-	fmt.Println(positiveTags, negativeTags)
-	fmt.Println()
+	// Walk through all files and append path which contains '.go'
+	filesPath, err := getFilesPath(path)
+	if err != nil {
+		return err
+	}
+
+	// find the file which corresponds to expression
+	files := []string{}
+	for _, path := range filesPath {
+		fileTags, hasTags, err := extractTags(path)
+		if err != nil {
+			return err
+		}
+		if hasTags {
+			isMatch := true
+			for _, pTag := range positiveTags {
+				contain := false
+				for _, fileTag := range fileTags {
+					if fileTag == pTag {
+						contain = true
+						break
+					}
+				}
+				if !contain {
+					isMatch = false
+					break
+				}
+			}
+			if !isMatch {
+				continue
+			}
+
+			for _, nTag := range negativeTags {
+				contain := false
+				for _, fileTag := range fileTags {
+					if fileTag == nTag {
+						contain = true
+						break
+					}
+				}
+				if contain {
+					isMatch = false
+					break
+				}
+			}
+			if !isMatch {
+				continue
+			}
+
+			files = append(files, path)
+		}
+	}
+
+	fmt.Println(files)
 
 	return nil
 }
@@ -154,24 +191,12 @@ func ListTags(path string) error {
 	// Add file name to tag key
 	tagsMap := map[string]bool{}
 	for _, path := range filesPath {
-		content, err := os.ReadFile(path)
+		tags, hasTags, err := extractTags(path)
 		if err != nil {
-			panic(err)
-		}
-		fileContent := string(content)
-		lines := strings.Split(fileContent, "\n")
-
-		// if not set tag, continue to next file
-		if len(lines) < 3 {
-			continue
+			return err
 		}
 
-		thirdLine := strings.TrimSpace(lines[2])
-		if strings.HasPrefix(thirdLine, "/* @tags:") && strings.HasSuffix(thirdLine, "*/") {
-			tagsLine := strings.TrimPrefix(thirdLine, "/* @tags:")
-			tagsLine = strings.TrimSuffix(tagsLine, "*/")
-			tagsLine = strings.TrimSpace(tagsLine)
-			tags := strings.Split(tagsLine, ",")
+		if hasTags {
 			for _, tag := range tags {
 				tagsMap[tag] = true
 			}
@@ -216,6 +241,64 @@ func getFilesPath(path string) ([]string, error) {
 	})
 
 	return filesPath, err
+}
+
+func getTagFiles(filesPath []string) (map[string][]string, error) {
+	tagFiles := map[string][]string{}
+	for _, path := range filesPath {
+		content, err := os.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+		fileContent := string(content)
+		lines := strings.Split(fileContent, "\n")
+
+		// if not set tag, continue to next file
+		if len(lines) < 3 {
+			continue
+		}
+
+		thirdLine := strings.TrimSpace(lines[2])
+		if strings.HasPrefix(thirdLine, "/* @tags:") && strings.HasSuffix(thirdLine, "*/") {
+			tagsLine := strings.TrimPrefix(thirdLine, "/* @tags:")
+			tagsLine = strings.TrimSuffix(tagsLine, "*/")
+			tagsLine = strings.TrimSpace(tagsLine)
+			tags := strings.Split(tagsLine, ",")
+			for _, tag := range tags {
+				if _, ok := tagFiles[tag]; !ok {
+					tagFiles[tag] = []string{}
+				}
+				tagFiles[tag] = append(tagFiles[tag], path)
+			}
+		}
+	}
+
+	return tagFiles, nil
+}
+
+func extractTags(path string) ([]string, bool, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, false, err
+	}
+	fileContent := string(content)
+	lines := strings.Split(fileContent, "\n")
+
+	// if not set tag, continue to next file
+	if len(lines) < 3 {
+		return nil, false, nil
+	}
+
+	thirdLine := strings.TrimSpace(lines[2])
+	if strings.HasPrefix(thirdLine, "/* @tags:") && strings.HasSuffix(thirdLine, "*/") {
+		tagsLine := strings.TrimPrefix(thirdLine, "/* @tags:")
+		tagsLine = strings.TrimSuffix(tagsLine, "*/")
+		tagsLine = strings.TrimSpace(tagsLine)
+		tags := strings.Split(tagsLine, ",")
+		return tags, true, nil
+	}
+
+	return nil, false, nil
 }
 
 // Tag Statistics/Analytics: Providing insights and analytics related to tag usage, such as popular tags, tag frequency, or tag-based trends.
